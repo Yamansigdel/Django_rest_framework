8c8f8560edeab1ce0ab78a7d668a596053163d96
Yaman Sigdel <Yamansigdel999@gmail.com>
2023-12-13 21:37:40 +0545

    dj-rest

--- core/home/admin.py
+++ core/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from home.models import Student
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('name','age','father_name')
+
+admin.site.register(Student,StudentAdmin)
