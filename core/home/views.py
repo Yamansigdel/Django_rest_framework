6bfc2beba3745366934649b8503205c6b3f4917b
Yaman Sigdel <Yamansigdel999@gmail.com>
2023-12-13 22:36:32 +0545

    dj-rest

--- core/home/views.py
+++ core/home/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import *
+from .serailizers import * 
 
 @api_view(['GET'])
 def home(request):
-    if request.method=='GET':
-        return Response({'status': 200, 'message': 'Hello from django_rest_framework'})
+    student_objs=Student.objects.all()
+    serializer=StudentSerializer(student_objs,many=True)
+    return Response({'status': 200, 'payload': serializer.data})
 
+@api_view(['POST'])
+def post_student(request):
+    data=request.data
+    serializer=StudentSerializer(data)
+    if not serializer.is_valid():
+        return Response({'status': 403 ,'message':'something went wrong'})
+    
+    serializer.save()
+    return Response({'status':200, 'payload': serializer.data,'message':'you send'})
