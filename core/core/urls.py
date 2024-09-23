ec54994e12af4bd507f9d8cea4ea630839535d8f
Yaman Sigdel <Yamansigdel999@gmail.com>
2024-09-23 22:01:34 +0545

    dynamic pdf

--- core/core/urls.py
+++ core/core/urls.py
@@ -29,3 +29,15 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+#generate dynamic pdf 
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns+=staticfiles_urlpatterns()
